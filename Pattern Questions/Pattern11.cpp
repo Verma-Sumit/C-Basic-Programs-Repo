135be7fe4f5645d3c314e07dbcfa383327ec9990
Verma-Sumit <s.officialverma@gmail.com>
2024-05-30 15:59:55 +0530

    Update Pattern11.cpp

---
+++ Pattern Questions/Pattern11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+/*
+Pattern to be printed is:
+1
+2 1
+3 2 1
+4 3 2 1
+*/
+cout << "Enter the value of n: ";
+int n;
+cin >> n;
+int i = 1;
+while (i <= n){
+    int j = 1;
+    while (j <= i){
+        cout << (i-j+1) << " ";
+        j++;
+    }
+    cout << endl;
+    i++;
+}
+
+}
